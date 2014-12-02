b48d2f60c2ea81aa8a50c46607ce579d484f71e9
Radek Adamus <adamus.r@gmail.com>
2014-12-02 20:29:08 +0100

    adding the ability to load configuration from ec2 metadata

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-var helpers = require("./helpers");
-var AWS = require("aws-sdk");
-var Queue = require("queuemanager");
-var SQSCommand = require("./sqscommand");
-var SQSConsole = require("./sqsconsole");
+(function() 
+{
+	var helpers = require("./helpers");
+	var AWS = require("aws-sdk");
+	var Queue = require("queuemanager");
+	var SQSCommand = require("./sqscommand");
+	var SQSConsole = require("./sqsconsole");
 
+	var AWS_CONFIG_FILE = "./config.json";
+	var APP_CONFIG_FILE = "./app.json";
 
-var AWS_CONFIG_FILE = "./config.json";
-var APP_CONFIG_FILE = "./app.json";
+	var initConsole = function() {
+			var appConfig = helpers.readJSONFile(APP_CONFIG_FILE);
+			var queue = new Queue(new AWS.SQS(), appConfig.QueueUrl);
+			var sqsCommand = new SQSCommand(queue);
+			new SQSConsole(sqsCommand);
+	}
+	
+	var initAWS = function(start) {
+		var EC2MCred = new AWS.EC2MetadataCredentials();
 
+		EC2MCred.refresh(function(err){
+			if(err){
+				console.log(err +" - loading config from file " + AWS_CONFIG_FILE);
+				AWS.config.loadFromPath(AWS_CONFIG_FILE);								
+			}
+			start();
+		});
+	}
+	
+	initAWS(initConsole);
+		
 
-AWS.config.loadFromPath(AWS_CONFIG_FILE);
-
-var appConfig = helpers.readJSONFile(APP_CONFIG_FILE);
-
-var queue = new Queue(new AWS.SQS(), appConfig.QueueUrl);
-
-
-var sqsCommand = new SQSCommand(queue);
-
-
-new SQSConsole(sqsCommand);
-
+})()
