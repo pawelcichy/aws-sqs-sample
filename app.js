4e0bd1877c7edb7c100630ad15b21c21040b82f0
Radek Adamus <adamus.r@gmail.com>
2014-12-09 15:24:24 +0100

    refactoring aws initialization to separate script

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 (function() 
 {
 	var helpers = require("./helpers");
-	var AWS = require("aws-sdk");
 	var Queue = require("queuemanager");
 	var SQSCommand = require("./sqscommand");
 	var SQSConsole = require("./sqsconsole");
@@ -9,26 +8,15 @@
 	var AWS_CONFIG_FILE = "./config.json";
 	var APP_CONFIG_FILE = "./app.json";
 
-	var initConsole = function() {
+	var initConsole = function(AWS) {
 			var appConfig = helpers.readJSONFile(APP_CONFIG_FILE);
 			var queue = new Queue(new AWS.SQS(), appConfig.QueueUrl);
 			var sqsCommand = new SQSCommand(queue);
 			new SQSConsole(sqsCommand);
 	}
+		
 	
-	var initAWS = function(start) {
-		var EC2MCred = new AWS.EC2MetadataCredentials();
-
-		EC2MCred.refresh(function(err){
-			if(err){
-				console.log(err +" - loading config from file " + AWS_CONFIG_FILE);
-				AWS.config.loadFromPath(AWS_CONFIG_FILE);								
-			}
-			start();
-		});
-	}
-	
-	initAWS(initConsole);
+	require("./awshelper").initAWS(initConsole, AWS_CONFIG_FILE);
 		
 
 })()
